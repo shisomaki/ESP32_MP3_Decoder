a9b6c2fe85e84f512370c643b9dcecf4ffbde5dc
esa-n <moonrise256@gmail.com>
2018-07-26 19:13:53 +0900

    http.c: Clean header parse

--- components/http/http.c
+++ components/http/http.c
@@ -96,90 +96,66 @@ void metaint_parser(char recv_buf, int *s)
     switch (*s) {
         case 0:
             if (recv_buf == 'i')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 1:
             if (recv_buf == 'c')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 2:
             if (recv_buf == 'y')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 3:
             if (recv_buf == '-')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 4:
             if (recv_buf == 'm')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 5:
             if (recv_buf == 'e')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 6:
             if (recv_buf == 't')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 7:
             if (recv_buf == 'a')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 8:
             if (recv_buf == 'i')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 9:
             if (recv_buf == 'n')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 10:
             if (recv_buf == 't')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 11:
             if (recv_buf == ':')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 12:
             if (recv_buf == ' ')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 14:
             if (recv_buf == '\r')
-                (*s)++;
-            break;
-
-        default:
-            break;
+                goto SPP;
+            return;
     }
+
+    *s = 0;
+    return;
+SPP:
+    (*s)++;
 }
 
 void body_parser(char recv_buf, int *s)
@@ -187,34 +163,28 @@ void body_parser(char recv_buf, int *s)
     switch (*s) {
         case 0:
             if (recv_buf == '\r')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 1:
             if (recv_buf == '\n')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 2:
             if (recv_buf == '\r')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 3:
             if (recv_buf == '\n')
-                (*s)++;
-            else
-                *s = 0;
+                goto SPP;
             break;
         case 4:
-            (*s)++;
-
-        default:
-            break;
+            goto SPP;
     }
+
+    *s = 0;
+    return;
+SPP:
+    (*s)++;
 }
 #endif
 /**
